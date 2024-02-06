0a805a0ea67ad2b34b49ecc77876b42df0eac0f2
killer78887 <rohansahu700@gmail.com>
2024-02-06 20:27:32 +0530

    update

--- main.js
+++ main.js
@@ -2,15 +2,20 @@ const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
 let xoff = 0;
-
+let frame = 1;
 function draw(){
-    xoff+=0.01;
-    let x = noise(xoff)*canvas.width;
+    xoff = 0+0.01*frame;
     ctx.clearRect(0,0,canvas.width,canvas.height);
     ctx.fillStyle = "black";
     ctx.fillRect(0,0,canvas.width,canvas.height);
+    for (var i = 0; i < canvas.width; i++) {
+    let x = noise(xoff)*canvas.height;
+    xoff+=0.01;
+    
     ctx.fillStyle = "red";
     ctx.beginPath();
-    ctx.arc(x,200,5,0,2 * Math.PI);
+    ctx.arc(i,x,1,0,2 * Math.PI);
     ctx.fill();
+    }
+    frame++;
 }
